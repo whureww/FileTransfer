9ca5d511e405071483cd1986aeae40b9dda59976
晓秋sliy <3208361197@qq.com>
2026-08-02 11:57:41 +0800

    fix: 解决 kotlin-android 插件版本冲突
    - settings.gradle.kts: 移除 plugins 块 (classpath 已有 kotlin-android, 版本冲突)
    - build.gradle.kts: 添加 kotlinAndroid apply false (统一版本管理)
    - 版本由 libs.versions.toml 版本目录统一提供

--- Mobile/settings.gradle.kts
+++ Mobile/settings.gradle.kts
@@ -4,9 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins {
-        id("org.jetbrains.kotlin.android") version "2.0.20"
-    }
 }
 
 dependencyResolutionManagement {
